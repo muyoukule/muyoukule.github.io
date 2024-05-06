977a20a4fd48da7bfa075c81fdbe3aca3732fa73
muyoukule <943495963@qq.com>
2024-05-06 18:41:27 +0800

    Site updated: 2024-05-06 18:41:27

--- js/muyoukule.js
+++ js/muyoukule.js
@@ -124,3 +124,15 @@ function createtime() {
 setInterval(() => {
     createtime();
 }, 1000);
+
+
+if (document.querySelector('#bber-talk')) {
+    var swiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            pauseOnMouseEnter: true
+        },
+    });
+}
