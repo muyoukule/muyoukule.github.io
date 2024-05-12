8284f100be4b6d1491631c711605d7adfe029f38
muyoukule <943495963@qq.com>
2024-05-12 12:50:10 +0800

    Site updated: 2024-05-12 12:50:10

--- js/muyoukule.js
+++ js/muyoukule.js
@@ -132,12 +132,11 @@ function formatISODateTime(isoDate) {
     const year = date.getUTCFullYear();
     const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); // 月份是从0开始的
     const day = date.getUTCDate().toString().padStart(2, '0');
-    // const hours = date.getUTCHours().toString().padStart(2, '0');
-    // const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-    // const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = date.getUTCSeconds().toString().padStart(2, '0');
 
-    // return `${year}/${month}/${day}  ${hours}:${minutes}:${seconds}`;
-    return `${year}/${month}/${day}`;
+    return `${year}/${month}/${day}  ${hours}:${minutes}:${seconds}`;
   }
 
   const changeTimeInEssay = () => {
